f2fe60f6208d145761475f9346e79ef2ebcbd9c0
lunshwashakya <lunshwa.shakya95@gmail.com>
2024-11-17 21:30:08 +1100

    update

---
+++ day_02/app.js
@@ -0,0 +1,50 @@
+const nameOfDay = "Monday";
+
+if (nameOfDay === "Monday") {
+    console.log("Today is Monday");
+    console.log("Today is weekday!");
+}
+else if (nameOfDay === "Tuesday") {
+    console.log("Today is Tuesday");
+    console.log("Today is weekday!");
+}
+else if (nameOfDay === "Wednesday") {
+    console.log("Today is Wednesday");
+    console.log("Today is weekday!");
+}
+else if (nameOfDay === "Thursday") {
+    console.log("Today is Thursday");
+    console.log("Today is weekday!");
+}
+else if (nameOfDay === "Friday") {
+    console.log("Today is Friday");
+    console.log("Today is weekday!");
+}
+else {
+    console.log("Today is weekend!");
+}
+    
+switch(nameOfDay){
+    case "Monday":
+        console.log("Today is Monday");
+        console.log("Today is weekday!");
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        console.log("Today is weekday!");
+        break;
+    case "Wednesday":
+        console.log("Today is Wednesday");
+        console.log("Today is weekday!");
+        break;
+    case "Thursday":
+        console.log("Today is Thursday");
+        console.log("Today is weekday!");
+        break;
+    case "Friday":
+        console.log("Today is Friday");
+        console.log("Today is weekday!");
+        break;
+    default:
+        console.log("Today is weekday!");
+}
